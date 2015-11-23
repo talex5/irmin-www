c155980fdb3bef6815d3411bb8ef3940910371a0
Mindy Preston <meetup@yomimono.org>
2015-11-23 14:20:23 +0000

    Minor changes to comply with new Irmin API; dead code for generating init_db.ml

--- unikernel.ml
+++ unikernel.ml
@@ -16,8 +16,9 @@ module Context = struct
   let v () = failwith "Context"
 end
 
+module Hash = Irmin.Hash.SHA1
 module Mirage_git_memory = Irmin_mirage.Irmin_git.Memory(Context)(Git.Inflate.None)
-module Store = Irmin.Basic(Mirage_git_memory)(Irmin.Contents.String)
+module Store = Irmin_mem.Make(Irmin.Contents.String)(Irmin.Ref.String)(Hash)
 let config = Irmin_mem.config ()
 
 (*
@@ -25,7 +26,7 @@ module Store = Irmin.Basic(Irmin_unix.Irmin_git.FS)(Irmin.Contents.String)
 let config = Irmin_unix.Irmin_git.config ~root:"db" ()
 *)
 
-module Bundle = Tc.Pair(Store.Private.Slice)(Store.Head)
+module Bundle = Tc.Pair(Store.Private.Slice)(Hash)
 
 (* Split a URI into a list of path segments *)
 let split_path path =
@@ -72,62 +73,70 @@ module Main (Stack:STACKV4) (Conf:KV_RO) (Clock:V1.CLOCK) = struct
     Store.head s >>= function
     | None -> failwith "dump: no head!"
     | Some head ->
-    Store.export ~max:[head] s >>= fun slice ->
-    let bundle = (slice, head) in
-    let buf = Cstruct.create (Bundle.size_of bundle) in
-    let rest = Bundle.write bundle buf in
-    assert (Cstruct.len rest = 0);
-    let path = "init_db.ml" in
-    Printf.printf "Writing %s...\n%!" path;
-    let ch = open_out_bin path in
-    Printf.fprintf ch "let init_db = %S" (Cstruct.to_string buf);
-    close_out ch;
-    Printf.printf "Wrote %s\n%!" path;
-    return ()
+      let r = Store.repo s in
+      Store.Repo.export ~max:[head] r >>= fun slice ->
+      let bundle = (slice, head) in
+      let buf = Cstruct.create (Bundle.size_of bundle) in
+      let rest = Bundle.write bundle buf in
+      assert (Cstruct.len rest = 0);
+      let path = "init_db.ml" in
+      Printf.printf "Writing %s...\n%!" path;
+      let ch = open_out_bin path in
+      Printf.fprintf ch "let init_db = %S" (Cstruct.to_string buf);
+      close_out ch;
+      Printf.printf "Wrote %s\n%!" path;
+      return ()
+
+  let make_source () =
+    let contents = "<body><h1>It works!</h1></body>" in
+    let path = ["index.html"] in
+    Store.Repo.create config >>= fun r ->
+    Store.master task r >>= fun s ->
+    Store.update (s "Default site") path contents >>= fun () ->
+    dump s
 
   let import s db =
     let s = s "import" in
     let buf = Mstruct.of_string db in
+    Printf.printf "db: [%s]\n" db;
+    Printf.printf "before bundle.read\n";
     let (slice, head) = Bundle.read buf in
-    Store.import s slice >>= function
+    Printf.printf "after bundle.read\n";
+    let r = Store.repo s in
+    Store.Repo.import r slice >>= function
     | `Error -> failwith "Irmin import failed"
     | `Ok ->
     Store.fast_forward_head s head >>= function
-    | false -> failwith "Irmin import failed at FF"
+    | false -> failwith ("Irmin import failed at fast-forward for head " ^
+                         (Hash.to_hum head))
     | true -> return ()
 
   let start stack conf _clock =
     X509.certificate conf `Default >>= fun cert ->
     let tls_config = Tls.Config.server ~certificates:(`Single cert) () in
-    Store.create config task >>= fun s ->
-(*     dump s >>= fun () -> *)
+    Store.Repo.create config >>= fun r ->
+    Printf.printf "mark 1\n";
+    Store.master task r >>= fun s ->
+    Printf.printf "mark 2\n";
     import s Init_db.init_db >>= fun () ->
+    Printf.printf "mark 3\n";
     let http = S.make ~conn_closed:ignore ~callback:(handle_request s) () in
+    Printf.printf "mark 4\n";
     Stack.listen_tcpv4 stack ~port:8443 (wrap_tls tls_config (S.listen http));
-    Lwt.async (fun () ->
-      let module Irmin_server = struct
-        (* We have to define this here because we need [stack] in scope
+    Printf.printf "mark 5\n";
+    let module Irmin_server = struct
+      (* We have to define this here because we need [stack] in scope
          * for [listen]. Perhaps it would make more sense for [Irmin_http_server]
          * to return the spec, rather than calling [listen] itself? *)
-        module X = struct
-          include S
+      module X = S
+      module Y = struct
+        let pretty d =
+          Printf.sprintf "%Ld" d
+      end
 
-          let listen t ?timeout uri =
-            assert (timeout = None);
-            let port = match Uri.port uri with
-              | None   -> failwith "Missing port in Irmin endpoint"
-              | Some p -> p in
-            Stack.listen_tcpv4 stack ~port (wrap_tls tls_config (listen t));
-            return ()
-        end
-        module Y = struct
-          let pretty d =
-            Printf.sprintf "%Ld" d
-        end
-
-        include Irmin_http_server.Make(X)(Y)(Store)
-      end in
-      Irmin_server.listen (s "server") ~strict:true irmin_uri
-    );
+      include Irmin_http_server.Make(X)(Y)(Store)
+    end in
+    let spec = Irmin_server.http_spec (s "server") ~strict:true in
+    Stack.listen_tcpv4 stack ~port:8444 (wrap_tls tls_config (S.listen spec));
     Stack.listen stack
 end
